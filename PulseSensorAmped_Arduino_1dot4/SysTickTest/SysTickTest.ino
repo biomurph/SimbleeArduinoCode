361182542d09ce0174dae6f0f4c8f235701e1ae1
shadow85926 <shadowappdev@gmail.com>
2016-04-01 21:29:09 -0600

    UL
    Uploaded the code

---
+++ PulseSensorAmped_Arduino_1dot4/SysTickTest/SysTickTest.ino
@@ -0,0 +1,17 @@
+
+#include"mcg.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+pll_init(8000000, LOW_POWER ,CRYSTAL, 4, 24, MCGOUT);
+
+}
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
